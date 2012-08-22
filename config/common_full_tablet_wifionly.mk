98d52e10bd02e774e04c2ca3937dbd86f922d1f6
Paul Beeler <sparksco@gmail.com>
2012-08-21 22:05:04 -0600

    config full tablet wifi: Tweak tablet builds for grouper
        * Configure common_full_tablet_wifionly.mk to check for
          if grouper build to customize config from the device tree
    
    Change-Id: I9a1e978940b15a149bbf4f783f193b04712932ee

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,17 +1,21 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common_full.mk)
+ifeq ($(CM_BUILD),grouper)
+    $(call inherit-product, device/asus/grouper/config/grouper.mk)
+else
+    # Inherit common CM stuff
+    $(call inherit-product, vendor/cm/config/common_full.mk)
 
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=CyanTone.ogg \
-    ro.config.notification_sound=CyanMessage.ogg \
-    ro.config.alarm_alert=CyanAlarm.ogg
+    # Default ringtone
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.config.ringtone=CyanTone.ogg \
+        ro.config.notification_sound=CyanMessage.ogg \
+        ro.config.alarm_alert=CyanAlarm.ogg
 
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    # BT config
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+        system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
+
+    ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+        PRODUCT_COPY_FILES += \
+            vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+    endif
 endif
