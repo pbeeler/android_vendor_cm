532b63dee5850177a797886604260dcb8c165669
Paul Beeler <sparksco@gmail.com>
2012-09-05 22:28:11 -0600

    common config: change location of Terminal Emulator location
    Change-Id: I5d2b899cf1b99502dd22ee005eab92a3c57f5444
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -98,9 +98,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/bin/modelid_cfg.sh:system/bin/modelid_cfg.sh
 
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+# jackpal
+$(call inherit-product, vendor/jackpal/jackpal-vendor.mk)
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
